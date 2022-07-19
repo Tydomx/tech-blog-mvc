f62b36894880b45d7ecc07e20d92f1d4c99b583d
Tydomx <michaelthai16@gmail.com>
2022-07-19 17:11:40 -0500

    Added connection to server

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const sequelize = require('./config/connection');
+const path = require('path');
+// import helpers
+const helpers = require('./utils/helpers');
+// importing handlebars
+const exphbs = require('express-handlebars');
+// passing helpers to existing expbs.create()
+const hbs = exphbs.create({ helpers });
+// setting up express.js session and connect session to Sequelize DB
+const session = require('express-session');
+const SequelizeStore = requre('connect-session-sequelize')(session.Store);
+const sess = {
+	secret: 'Super secret secret',
+	cookie: {},
+	resave: false,
+	saveUninitialized: true,
+	store: new SequelizeStore({
+		db: sequelize
+	})
+};
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session(sess));
+
+// handlebars middleware
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+// turn on routes
+app.use(routes);
+
+// turn on connection to db and server
+sequelize.sync({ force: false }).then(() => {
+	app.listen(PORT, () => console.log(`Now listening on ${PORT}`));
+});
